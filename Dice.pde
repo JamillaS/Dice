f69aa58f0d7c1ba091e3d485d57a0c941f344277
JamillaS <jasitu1@s.sfusd.edu>
2015-09-10 10:34:21 -0700

    Di

--- Dice.pde
+++ Dice.pde
@@ -1,4 +1,5 @@
 
+int sum = 0;
 
 void setup()
 {
@@ -6,7 +7,10 @@ void setup()
 	size(400, 400);
 }
 void draw()
-{
+{	
+	background(0);
+	fill(255);
+	text("The total is " + sum, 20, 20);
 	for(int y = 50; y <= 300; y += 50)
 	{
 		for(int x = 50; x <= 300; x += 50)
@@ -15,13 +19,16 @@ void draw()
 			one.roll();
 			one.show();
 		}
+		
 	}
 	
 	//your code here
 }
 void mousePressed()
 {
+
 	redraw();
+
 }
 class Die //models one single dice cube
 {
@@ -33,56 +40,74 @@ class Die //models one single dice cube
 		myY = y;
 		numDots = (int)(Math.random() * 6 + 1);
 		//variable initializations here
+		
+		
 	}
 	void roll()
 	{
+		//sum = 0;
 		fill(0);
 		if(numDots == 1)
 		{
 			numDots = 1;
+			sum = sum + numDots;
+			
+
 		}
 		if(numDots == 2)
 		{
 			numDots = 2;
+			
+			
 		}
 		if(numDots == 3)
 		{
 			numDots = 3;
+			
 		}
 		if(numDots == 4)
 		{
 			numDots = 4;
+			
 		}
 		if(numDots == 5)
 		{
 			numDots = 5;
+			
 		}
 		if(numDots == 6)
 		{
 			numDots = 6;
+			
 		}
 		//your code here
+		
 	}
 	void show()
 	{
+
 		
 		fill(255);
 		rect(myX,myY,50, 50);
 		fill(0);
 		if(numDots == 1)
 		{
+
 			ellipse(myX+25, myY+25, 10, 10);
+			
 		}
 		if(numDots == 2)
 		{
 			ellipse(myX + 40, myY + 40, 10, 10);
 			ellipse(myX + 10, myY + 10, 10, 10);
+			
 		}
 		if(numDots == 3)
 		{
 			ellipse(myX + 40, myY + 40, 10, 10);
 			ellipse(myX + 10, myY + 10, 10, 10);
 			ellipse(myX + 25, myY + 25, 10, 10);
+			
 		}
 		if(numDots == 4)
 		{
@@ -90,6 +115,7 @@ class Die //models one single dice cube
 			ellipse(myX + 10, myY + 40, 10, 10);
 			ellipse(myX + 40, myY + 10, 10, 10);
 			ellipse(myX + 40, myY + 40, 10, 10);
+			
 		}
 		if(numDots == 5)
 		{
@@ -98,6 +124,7 @@ class Die //models one single dice cube
 			ellipse(myX + 40, myY + 10, 10, 10);
 			ellipse(myX + 40, myY + 40, 10, 10);
 			ellipse(myX + 25, myY + 25, 10, 10);
+			
 		}
 		if(numDots == 6)
 		{
@@ -107,7 +134,9 @@ class Die //models one single dice cube
 			ellipse(myX + 40, myY + 40, 10, 10);
 			ellipse(myX + 10, myY + 25, 10, 10);
 			ellipse(myX + 40, myY + 25, 10, 10);
+			
 		}
+		
 		//your code here
 	}
 }
--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 public class Dice extends PApplet {
 
 
+int sum = 0;
 
 public void setup()
 {
@@ -22,7 +23,10 @@ public void setup()
 	size(400, 400);
 }
 public void draw()
-{
+{	
+	background(0);
+	fill(255);
+	text("The total is " + sum, 20, 20);
 	for(int y = 50; y <= 300; y += 50)
 	{
 		for(int x = 50; x <= 300; x += 50)
@@ -31,13 +35,16 @@ public void draw()
 			one.roll();
 			one.show();
 		}
+		
 	}
 	
 	//your code here
 }
 public void mousePressed()
 {
+
 	redraw();
+
 }
 class Die //models one single dice cube
 {
@@ -49,56 +56,74 @@ class Die //models one single dice cube
 		myY = y;
 		numDots = (int)(Math.random() * 6 + 1);
 		//variable initializations here
+		
+		
 	}
 	public void roll()
 	{
+		//sum = 0;
 		fill(0);
 		if(numDots == 1)
 		{
 			numDots = 1;
+			sum = sum + numDots;
+			
+
 		}
 		if(numDots == 2)
 		{
 			numDots = 2;
+			
+			
 		}
 		if(numDots == 3)
 		{
 			numDots = 3;
+			
 		}
 		if(numDots == 4)
 		{
 			numDots = 4;
+			
 		}
 		if(numDots == 5)
 		{
 			numDots = 5;
+			
 		}
 		if(numDots == 6)
 		{
 			numDots = 6;
+			
 		}
 		//your code here
+		
 	}
 	public void show()
 	{
+
 		
 		fill(255);
 		rect(myX,myY,50, 50);
 		fill(0);
 		if(numDots == 1)
 		{
+
 			ellipse(myX+25, myY+25, 10, 10);
+			
 		}
 		if(numDots == 2)
 		{
 			ellipse(myX + 40, myY + 40, 10, 10);
 			ellipse(myX + 10, myY + 10, 10, 10);
+			
 		}
 		if(numDots == 3)
 		{
 			ellipse(myX + 40, myY + 40, 10, 10);
 			ellipse(myX + 10, myY + 10, 10, 10);
 			ellipse(myX + 25, myY + 25, 10, 10);
+			
 		}
 		if(numDots == 4)
 		{
@@ -106,6 +131,7 @@ class Die //models one single dice cube
 			ellipse(myX + 10, myY + 40, 10, 10);
 			ellipse(myX + 40, myY + 10, 10, 10);
 			ellipse(myX + 40, myY + 40, 10, 10);
+			
 		}
 		if(numDots == 5)
 		{
@@ -114,6 +140,7 @@ class Die //models one single dice cube
 			ellipse(myX + 40, myY + 10, 10, 10);
 			ellipse(myX + 40, myY + 40, 10, 10);
 			ellipse(myX + 25, myY + 25, 10, 10);
+			
 		}
 		if(numDots == 6)
 		{
@@ -123,7 +150,9 @@ class Die //models one single dice cube
 			ellipse(myX + 40, myY + 40, 10, 10);
 			ellipse(myX + 10, myY + 25, 10, 10);
 			ellipse(myX + 40, myY + 25, 10, 10);
+			
 		}
+		
 		//your code here
 	}
 }
